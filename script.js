4bfb92595b2e605a4297cd3a872682dd09615f32
unknown <princeparco12@gmail.com>
2024-02-12 10:16:58 +0800

    added new

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.getElementById('siBtn').addEventListener('click', function() {
 
     // Mostrar el mensaje específico
     document.getElementById('messageContainer').style.display = 'block';
-    document.getElementById('messageContainer').innerHTML = 'YEHEEEEEYYYYYYYY';
+    document.getElementById('messageContainer').innerHTML = 'YEHEEEEEYYYYYYYY THANKS BON';
 
     // Mostrar otro gif después de 3 segundos
     setTimeout(function() {
